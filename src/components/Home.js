ea4e6df5175037a6f6a2233a5c73b4c201ffa8f8
Ali-khan15 <ak8855105@gmail.com>
2024-10-15 03:20:32 +0500

    first commit home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,42 @@
-import React from 'react'
-
+import React from 'react';
+import './Home.css'; // Import the CSS file
+ 
 function Home() {
   return (
-    <div>
-      <h1>Welcome to Calling APP succesfully Login</h1>
+    <div className="container">
+      <h1 className="heading">AI Phone Calls.</h1>
+      AUTOMATE YOUR PHONE CALLS WITH AI
+      <hr className="line" />
+ 
+      <textarea
+        className="script-box"
+        placeholder="Write your calling script here"
+      ></textarea>
+ 
+      <button className="submit-btn">Submit Script</button>
+ 
+      {/* Box container for phone numbers and CSV */}
+      <div className="number-container">
+        <div className="input-fields">
+          <input type="text" className="number-input" placeholder="Write number to call" />
+          <input type="text" className="number-input" placeholder="Write number to call" />
+          <input type="text" className="number-input" placeholder="Write number to call" />
+          <input type="text" className="number-input" placeholder="Write number to call" />
+          <input type="text" className="number-input" placeholder="Write number to call" />
+        </div>
+ 
+        {/* Plus sign */}
+        <div className="add-more">
+         
+          <input type="file" accept=".csv" className="csv-input" />
+        </div>
+ 
+        {/* Call button */}
+        <button className="submit-btn">Call</button>
+      </div>
     </div>
-  )
+  );
 }
-
-export default Home
+ 
+export default Home;
+ 
